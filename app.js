1702f321fbb42b6ad12a230551a6bf8251b6f8f7
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-22 14:53:59 -0700

    Added functions to track clicks :D

--- app.js
+++ app.js
@@ -28,8 +28,54 @@ var usedImages = [];
 function Image (name, type) {
   this.name = name;
   this.path = `./img/${name}.${type}`;
+  this.views = 0;
+  this.clicks = 0;
 }
 
-for (var i = 0; i < imageArray.length - 2; i++) {
-  imageObjects.push(new Image(imageArray[i]), 'jpg');
+function populateImages () {
+  for (var i = 0; i < imageArray.length - 2; i++) {
+    imageObjects.push(new Image(imageArray[i]), 'jpg');
+  }
+  imageObjects.push(new Image('usb', 'gif'));
+  imageObjects.push(new Image('sweep', 'png'));
 }
+
+function randImage () {
+  while (true) {
+    var randNum = Math.floor(Math.random * imageArray.length);
+    if (!usedImages.includes(imageObjects[randNum])) {
+      return imageObjects[randNum];
+    }
+  }
+}
+
+function newImage () {
+  var allPics = document.getElementsByClassName('className');
+  var newUsed = [];
+  for (var i = 0; i < allPics.length; i++) {
+    var tempImage = randImage();
+    tempImage.views += 1;
+    allPics[i].setAttribute('src', tempImage.path);
+    newUsed.push(tempImage);
+  }
+  usedImages = newUsed;
+}
+
+function imageClick () {
+  for (var i = 0; i < usedImages.length; i++) {
+    if (this.getAttribute('src') === usedImages[i].path) {
+      usedImages.clicks += 1;
+      newImage();
+    }
+  }
+}
+
+function addHandler() {
+  var allPics = document.getElementsByClassName('className');
+  for (var i = 0; i < allPics.length; i++) {
+    allPics[i].addEventHandler('click', imageClick);
+  }
+}
+
+populateImages();
+addHandler();
