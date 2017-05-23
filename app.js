2c03f110ba693f200c84ad01bb6445f30005ccd6
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-22 20:32:47 -0700

    Added documentation

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 'use strict';
-
+//Initial array containing image names used to construct objects
 var imageArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass', 'sweep', 'usb'];
-var imageObjects = [];
-var usedImages = [];
-var totalCounter = 0;
+var imageObjects = [];//Will house contructed objects
+var usedImages = [];//Will house previous guesses
+var totalCounter = 0;//Counts total number of clicks
 
 function Image (name, type) {
+  //Constructs image with name, file path and number of clicks/views
   this.name = name;
   this.path = `./img/${name}.${type}`;
   this.views = 0;
@@ -13,18 +14,22 @@ function Image (name, type) {
 }
 
 function populateImages () {
+  //Iterates throug imageArray and runs names through constructor.
   for (var i = 0; i < imageArray.length - 2; i++) {
     imageObjects.push(new Image(imageArray[i], 'jpg'));
   }
+  //Constructs the two non-jpg objects
   imageObjects.push(new Image('usb', 'gif'));
   imageObjects.push(new Image('sweep', 'png'));
 }
 
 function randImage () {
+  //Generates a random image from imageObjects array
   //console.log('working');
-  while (true) {
-    var randNum = Math.floor(Math.random() * imageArray.length);
+  while (true) { //Intentional infinite loop to keep generating random numbers until a proper choice is made.
+    var randNum = Math.floor(Math.random() * imageArray.length); //Generates a random number between 0 and 19 that will be used as the index for imageObjects
     //console.log(randNum);
+    //Checks to see if random Object is in list of previous guesses.
     if (usedImages.includes(imageObjects[randNum]) === false) {
       //console.log(imageObjects[randNum]);
       return imageObjects[randNum];
@@ -33,33 +38,41 @@ function randImage () {
 }
 
 function newImage () {
+  //Retrieves all image elements in for survey
   var allPics = document.getElementsByClassName('randPic');
+  //Stores 3 new Objects.
   var newUsed = [];
   for (var i = 0; i < allPics.length; i++) {
+    //Assigns a temporary variable name to random object
     var tempImage = randImage();
+    //increases views of temp Object
     tempImage.views += 1;
+    //Assigns Object path as an image source
     allPics[i].setAttribute('src', tempImage.path);
     newUsed.push(tempImage);
-    usedImages.push(tempImage);
+    usedImages.push(tempImage); //Pushes tempImage to usedImages to prevent it from being drawn again
     //console.log(tempImage);
   }
   //console.log(newUsed);
-  usedImages = newUsed;
+  usedImages = newUsed; //reassigns used images to the new array of used objects
   //console.log(usedImages);
 }
 
 function updateHTMLTotal() {
+  //Upates the total counter on HTML page
   document.getElementsByTagName('h3')[0].innerHTML = `Total number of clicks: ${totalCounter}`;
 }
 
 function imageClick () {
+  //Iterates through old values
+  //If source of clicked image is the same as the objects path, it's clicked counter increases by one.
   for (var i = 0; i < usedImages.length; i++) {
     if (this.getAttribute('src') === usedImages[i].path) {
       usedImages[i].clicks += 1;
       //console.log('Number of clicks: ', usedImages[i].clicks);
       //console.log('total: ', totalCounter);
       totalCounter += 1;
-      newImage();
+      newImage(); //New images are generated
     }
   }
   endSurvey();
@@ -68,6 +81,7 @@ function imageClick () {
 }
 
 function postResults () {
+  //Iterates through clicked values of objects and appends them to HTML ul element
   var listArray = [];
   var masterList = document.getElementById('results');
   for (var i = 0; i < imageObjects.length; i++) {
@@ -77,6 +91,9 @@ function postResults () {
 }
 
 function endSurvey () {
+  //Checks to see if totCounter is greater than 25
+  //If so, the event handler is removed from images to prevent clicking
+  //postResults is called to display results on HTML
   if (totalCounter >= 25) {
     var allPics = document.getElementsByClassName('randPic');
     for (var i = 0; i < allPics.length; i++) {
@@ -88,6 +105,7 @@ function endSurvey () {
 }
 
 function addHandler() {
+  //Adds event handler to image elements
   var allPics = document.getElementsByClassName('randPic');
   for (var i = 0; i < allPics.length; i++) {
     //console.log(allPics[i]);
